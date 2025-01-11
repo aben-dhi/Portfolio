fc2128f69273b756def5695dba1994c4374c22a6
aben-dhi <bdamoura8@gmail.com>
2025-01-11 07:16:16 +0100

    fixed

--- src/sections/Contact/Contact.jsx
+++ src/sections/Contact/Contact.jsx
@@ -2,46 +2,35 @@ import styles from './ContactStyles.module.css';
 
 function Contact() {
   return (
-	<section id="contact" className={styles.container}>
-		<h1 className="sectionTitle">Contact</h1>
-		<form action="">
-			<div className="formGroup">
-				<label htmlFor="name" hidden>
-					Name
-				</label>
-				<input 
-					type="text" 
-					id="name" 
-					name="name" 
-					placeholder="Name" 
-					required 
-				/>
-			</div>
-			<div className="formGroup">
-				<label htmlFor="email" hidden>
-					email
-				</label>
-				<input 
-					type="text" 
-					id="email" 
-					name="email" 
-					placeholder="E-mail" 
-					required 
-				/>
-			</div>
-			<div className="formGroup">
-				<label htmlFor="message" hidden>
-					Message
-				</label>
-				<textarea 
-					id="message" 
-					name="message" 
-					placeholder="Message" 
-					required></textarea>
-			</div>
-			<input className="hover btn" type="submit" value="Submit"/>
-		</form>
-	</section>
+    <section id="contact" className={styles.container}>
+      <h1 className="sectionTitle">Contact</h1>
+      <form action="">
+        <div className="formGroup">
+          <label htmlFor="name" hidden>
+            Name
+          </label>
+          <input 
+            type="text" 
+            id="name" 
+            name="name" 
+            placeholder="Name" 
+            required 
+          />
+        </div>
+        <div className="formGroup">
+          <label htmlFor="email" hidden>
+            email
+          </label>
+          <input 
+            type="text" 
+            id="email" 
+            name="email" 
+            placeholder="E-mail" 
+            required
+          />
+        </div>
+      </form>
+    </section>
   );
 }
 
